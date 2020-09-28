8db11cf4d2e39f80d5c9235f03c5cd2a74ae5f4f
Willian Joaquim Barros Lúcio <willian_jbl@hotmail.com>
2020-09-28 18:05:48 -0300

    implementada a tela de login

--- src/controllers/UserController.php
+++ src/controllers/UserController.php
@@ -9,7 +9,12 @@ class UserController extends Controller
 {
     public function signin()
     {
-        
+        $this->render('user/login');
+    }
+
+    public function signinAction()
+    {
+        echo 'Login recebido';
     }
 
     public function signup()
--- src/routes.php
+++ src/routes.php
@@ -5,4 +5,5 @@ $router = new Router();
 
 $router->get('/', 'HomeController@index');
 $router->get('/login', 'UserController@signin');
+$router->post('/login', 'UserController@signinAction');
 $router->get('/cadastro', 'UserController@signup');
