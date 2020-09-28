ce90b88decc19a7a19bc2ea3a1c302f95bdfdbf5
Willian Joaquim Barros Lúcio <willian_jbl@hotmail.com>
2020-09-28 15:50:31 -0300

    integrando MVC

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,9 @@
+<?php
+namespace src\controllers;
+
+use \core\Controller;
+use src\models\User;
+
+class HomeController extends Controller
+{
+}
---
+++ src/controllers/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace src\controllers;
+
+use core\Controller;
+use src\models\User;
+
+class UserController extends Controller
+{
+}
