438594b12bb160e879dada1a016f4814997f1789
Willian Joaquim Barros Lúcio <willian_jbl@hotmail.com>
2020-10-19 11:51:39 -0300

    refatoração de strings post-item

--- src/views/partials/feed/feed-post.php
+++ src/views/partials/feed/feed-post.php
@@ -2,18 +2,18 @@
     <div class="box-body">
         <div class="feed-new-editor m-10 row">
             <div class="feed-new-avatar">
-                <img src="<?= $base ?>/media/avatars/<?= $user->getAvatar() ?>" />
+                <img src="<?= "$base/media/avatars/$user->getAvatar()" ?>" />
             </div>
             <div class="feed-new-input-placeholder">O que você está pensando, <?= $user->getName() ?>?</div>
             <div class="feed-new-input" contenteditable="true"></div>
             <div class="feed-new-picture">
-                <img src="<?= $base ?>/assets/images/photo.png" />
+                <img src="<?= "$base/assets/images/photo.png" ?>" />
                 <input type="file" name="post-picture" id="post-picture" accept="image/png, image/jpeg, image/jpg, image/bmp">
             </div>
             <div class="feed-new-send">
-                <img src="<?= $base ?>/assets/images/send.png" />
+                <img src="<?= "$base/assets/images/send.png" ?>" />
             </div>
-            <form id="feed-new-form" method="POST" action="<?= $base ?>/post/new">
+            <form id="feed-new-form" method="POST" action="<?= "$base/post/new" ?>">
                 <input type="hidden" name="body">
             </form>
         </div>
