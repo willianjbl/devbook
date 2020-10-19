fe99f50c16a7a68ed1c1a6701403bd600e573a65
Willian Joaquim Barros Lúcio <willian_jbl@hotmail.com>
2020-10-19 11:58:21 -0300

    ajuste6

--- src/views/partials/feed/feed-post.php
+++ src/views/partials/feed/feed-post.php
@@ -2,7 +2,7 @@
     <div class="box-body">
         <div class="feed-new-editor m-10 row">
             <div class="feed-new-avatar">
-                <img src="<?= "$base/media/avatars/$user->getAvatar()" ?>" />
+                <img src="<?= "$base/media/avatars/{$user->getAvatar()}" ?>" />
             </div>
             <div class="feed-new-input-placeholder">O que você está pensando, <?= $user->getName() ?>?</div>
             <div class="feed-new-input" contenteditable="true"></div>
